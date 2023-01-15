512add05af5d1d850d45e7e8ecfb30d986c978ae
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2023-01-14 21:38:18 -0500

    Initial Commit

---
+++ app/src/main/java/edu/northeastern/numadsp_23vinhk/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.northeastern.numadsp_23vinhk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void HandleOnClick(View view){
+        Toast.makeText(this,"NAME:VINAYAKA H K \n EMAIL:hosahallikotrappa.v@northeastern.edu" ,Toast.LENGTH_LONG).show();
+
+    }
+}
