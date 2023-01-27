138c2b31a6d9be1ed07bcbec3380d2be194a1de8
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2023-01-27 18:00:46 -0500

    week 2

---
+++ app/src/main/java/edu/northeastern/numadsp_23vinhk/ClickyClickyActivity.java
@@ -0,0 +1,43 @@
+package edu.northeastern.numadsp_23vinhk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class ClickyClickyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+    }
+
+    public void onPress(View view) {
+        int theid = view.getId();
+        if (theid == R.id.butA)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:A");
+        }
+        if (theid == R.id.butB)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:B");
+        } if (theid == R.id.butC)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:C");
+        } if (theid == R.id.butD)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:D");
+        } if (theid == R.id.butE)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:E");
+        }
+        if (theid == R.id.butF)
+        {
+            ((TextView)findViewById(R.id.textView4)).setText("PRESSED:F");
+        }
+    }
+
+}
--- app/src/main/java/edu/northeastern/numadsp_23vinhk/MainActivity.java
+++ app/src/main/java/edu/northeastern/numadsp_23vinhk/MainActivity.java
@@ -2,6 +2,7 @@ package edu.northeastern.numadsp_23vinhk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,4 +19,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"NAME:VINAYAKA H K \n EMAIL:hosahallikotrappa.v@northeastern.edu" ,Toast.LENGTH_LONG).show();
 
     }
+
+    public void launchNewActivity(View view){
+        Intent i = new Intent(this,ClickyClickyActivity.class);
+        startActivity(i);
+    }
+
 }
